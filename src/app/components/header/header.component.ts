117345b60f86a3276dbaafacca340098f2da46a1
bmazza <bruno.mazza@eqs.com>
2022-03-25 16:36:39 +0100

    Add localization and assets.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+  styleUrls: ['./navigation.component.scss'],
 })
 export class NavigationComponent implements OnInit {
+  constructor(public translate: TranslateService) {
+    translate.addLangs(['en', 'de', 'it']);
+    translate.setDefaultLang('en');
 
-  constructor() { }
-
-  ngOnInit(): void {
+    const browserLang = translate.getBrowserLang()!;
+    translate.use(browserLang.match(/en|de|it|/) ? browserLang : 'en');
   }
 
+  ngOnInit(): void {}
 }
